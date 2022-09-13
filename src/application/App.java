5e7dbc486e625db376e46d15dc0a97532346a128
Danillo Moraes <moraesdanillo10@gmail.com>
2022-09-12 22:46:50 -0300

    Criação da estrutura Set e contagem dos usuários

--- src/application/App.java
+++ src/application/App.java
@@ -2,7 +2,14 @@ package application;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+import entities.Log;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -12,11 +19,25 @@ public class App {
         String arquivo = sc.nextLine();
 
         try (BufferedReader br = new BufferedReader(new FileReader(arquivo))) {
+
+            Set<Log> lista = new TreeSet<>();
             String linha = br.readLine();
+            
             while (linha != null) {
-                System.out.println(linha);
+            
+                String[] dado = linha.split(" ");
+                Date data = Date.from(Instant.parse(dado[1]));
+                lista.add(new Log(dado[0], data));
                 linha = br.readLine();
+
             }
+
+            for (Log log : lista) {
+                System.out.println(log);
+            }
+
+            System.out.println(lista.size());
+
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
         }
--- src/entities/Log.java
+++ src/entities/Log.java
@@ -2,7 +2,7 @@ package entities;
 
 import java.util.Date;
 
-public class Log {
+public class Log implements Comparable<Log>{
     private String nome;
     private Date data;
     
@@ -25,6 +25,48 @@ public class Log {
 
     public void setData(Date data) {
         this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Log other = (Log) obj;
+        if (data == null) {
+            if (other.data != null)
+                return false;
+        } else if (!data.equals(other.data))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Log [data=" + data + ", nome=" + nome + "]";
+    }
+
+    @Override
+    public int compareTo(Log o) {
+        // Set não admite repetições
+        return nome.toUpperCase().compareTo(o.getNome().toUpperCase());
     }
 
 }
